1815d42c8839c4d5b964f456241314afe43f1c2b
Junior <junior.presagios@gmail.com>
2020-08-22 17:34:55 -0400

    Test every GateWay can be retrieved

--- server/tests/gateway-repository.test.ts
+++ server/tests/gateway-repository.test.ts
@@ -14,7 +14,6 @@ afterEach(async () => await clearDatabase());
 // Finished to in-memory and closed resource
 afterAll(async () => await closeDatabase());
 
-import Device from "../src/models/device.model";
 import GateWay from "../src/models/gateways.model";
 import GateWayRepository from "../src/repositories/gateway-repository";
 
@@ -32,6 +31,18 @@ describe("GateWay Repository", () => {
   }, 9000);
 
   test("Method allGateWay successfully retrieved all GateWays", async () => {
+    const gateWayRepository = new GateWayRepository();
+
+    for (let i = 0; i < gateWayData.length; i++) {
+      await gateWayRepository.insertGateWay(gateWayData[i]);
+    }
+
+    const countGateWays = await gateWayRepository.allGateWay();
+
+    expect(countGateWays.length).toEqual(gateWayData.length);
+  }, 9000);
+
+  test("Compare allGateWay result with all GateWay inserted using deep comparation (field by field)", async () => {
     const gateWayRepository = new GateWayRepository();
 
     for (let i = 0; i < gateWayData.length; i++) {
