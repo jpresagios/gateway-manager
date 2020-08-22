fc4538d9a3257da91d75e28dd6e7b5954b993e43
Junior <junior.presagios@gmail.com>
2020-08-22 17:25:59 -0400

    Added initial tests

---
+++ server/tests/gateway-repository.test.ts
@@ -0,0 +1,45 @@
+import {
+  connect,
+  clearDatabase,
+  closeDatabase,
+} from "./mongodb-test-enviroment";
+import gateWayData from "./fake-data/gateway";
+
+// Connect to a new in-memory database before running any tests.
+beforeAll(async () => await connect());
+
+// Clear all test data after every test to have insolation database conditions
+afterEach(async () => await clearDatabase());
+
+// Finished to in-memory and closed resource
+afterAll(async () => await closeDatabase());
+
+import Device from "../src/models/device.model";
+import GateWay from "../src/models/gateways.model";
+import GateWayRepository from "../src/repositories/gateway-repository";
+
+describe("GateWay Repository", () => {
+  test("Method insertGateWay Successfully insert GateWays on Database", async () => {
+    const gateWayRepository = new GateWayRepository();
+
+    for (let i = 0; i < gateWayData.length; i++) {
+      await gateWayRepository.insertGateWay(gateWayData[i]);
+    }
+
+    const countGateWays: number = await GateWay.count({});
+
+    expect(countGateWays).toEqual(gateWayData.length);
+  }, 9000);
+
+  test("Method allGateWay successfully retrieved all GateWays", async () => {
+    const gateWayRepository = new GateWayRepository();
+
+    for (let i = 0; i < gateWayData.length; i++) {
+      await gateWayRepository.insertGateWay(gateWayData[i]);
+    }
+
+    const countGateWays = await gateWayRepository.allGateWay();
+
+    expect(countGateWays.length).toEqual(gateWayData.length);
+  }, 9000);
+});
