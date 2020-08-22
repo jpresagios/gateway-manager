7eb3b731259417bc4fe069197184314c268abf5c
Junior <junior.presagios@gmail.com>
2020-08-22 17:25:49 -0400

    Added gateway  fake data

---
+++ server/tests/fake-data/gateway.ts
@@ -0,0 +1,12 @@
+export default [
+  {
+    serialNumber: "AVGG1",
+    name: "GateWay 1",
+    ipV4: "69.89.31.226",
+  },
+  {
+    serialNumber: "ZVVF3",
+    name: "GateWay 2",
+    ipV4: "69.89.55.226",
+  },
+];
