4f8aa732cf88cc209e095285f4c4a735eb599584
Junior <junior.presagios@gmail.com>
2020-08-22 19:27:59 -0400

    Added endpoints for devices

---
+++ client/src/api/devices.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import env from "../configs/env-config";
+
+function deleteDevice(idDevice) {
+  return axios
+    .delete(`${env.REACT_APP_URL}/device/remove/${idDevice}`)
+    .then((res) => {
+      const { data } = res;
+      return data;
+    });
+}
+
+function createDevice(deviceData) {
+  return axios
+    .post(`${env.REACT_APP_URL}/device/add`, deviceData)
+    .then((res) => {
+      const { data } = res;
+      return data;
+    });
+}
+
+export { deleteDevice, createDevice };
