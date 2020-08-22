d4961bdd13f590ec8016960e2d5c0708d53b96df
Junior <junior.presagios@gmail.com>
2020-08-22 09:26:15 -0400

    Adde Device model

---
+++ server/src/models/device.model.ts
@@ -0,0 +1,48 @@
+import { Schema, model, Document } from "mongoose";
+import { IGateWay } from "./gateways.model";
+
+export interface IDevice extends Document {
+  gateWay: IGateWay["_id"];
+  uid: {
+    type: number;
+    required: true;
+  };
+
+  vendor: {
+    type: string;
+  };
+
+  createAt: {
+    type: Date;
+  };
+
+  status: {
+    type: string;
+    required: true;
+  };
+}
+
+const DeviceSchema: Schema = new Schema({
+  gateWay: { type: Schema.Types.ObjectId, ref: "GateWay" },
+  uid: {
+    type: Number,
+    required: true,
+  },
+
+  vendor: {
+    type: String,
+    required: true,
+  },
+
+  createAt: {
+    type: Date,
+    default: Date.now,
+  },
+
+  status: {
+    type: String,
+    required: true,
+  },
+});
+
+export default model<IDevice>("Device", DeviceSchema);
