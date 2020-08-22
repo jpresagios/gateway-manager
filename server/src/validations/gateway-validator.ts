4c8f5cdc59b9f98d1723c675064c11e1e20cd2c4
Junior <junior.presagios@gmail.com>
2020-08-22 16:58:18 -0400

    Added Gateway Validator

--- server/src/validations/gateway-validator.ts
+++ server/src/validations/gateway-validator.ts
@@ -1,30 +1,57 @@
-import { isEmpty, isIpV4 } from "./validators";
+import Validator from "./validators";
+import { TYPES } from "../types/types";
+import { inject, injectable } from "inversify";
+import IGateWayRepository from "../interfaces/igateway-repository";
+import IValidator from "../interfaces/validators/ivalidator-interface";
 
-const validateGateWay = (data) => {
-  const { serialNumber, name, ipV4 } = data;
+@injectable()
+export default class GateWayValidator extends Validator implements IValidator {
+  private gateWayRepository: IGateWayRepository;
 
-  const errors = {};
-
-  if (isEmpty(serialNumber)) {
-    errors["serialNumber"] = "serialNumber field is required";
+  constructor(
+    @inject(TYPES.GateWayRepository)
+    gateWayRepository: IGateWayRepository
+  ) {
+    super();
+    this.gateWayRepository = gateWayRepository;
   }
 
-  if (isEmpty(name)) {
-    errors["name"] = "name field is required";
-  }
+  isUnique = async (value: string): Promise<boolean> => {
+    const result = await this.gateWayRepository.getGateWayBySerialNumber(value);
 
-  if (isEmpty(ipV4)) {
-    errors["ipV4"] = "ipV4 field is required";
-  }
-
-  if (ipV4 && !isIpV4(ipV4)) {
-    errors["ipV4"] = "Required valid IpV4";
-  }
-
-  return {
-    errors,
-    isValid: isEmpty(errors),
+    return result.length === 0;
   };
-};
 
-export default validateGateWay;
+  /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+  validate = async (data) => {
+    const { serialNumber, name, ipV4 } = data;
+
+    const errors = {};
+
+    if (this.isEmpty(serialNumber)) {
+      errors["serialNumber"] = "serialNumber field is required";
+    } else {
+      const isUniqueSerialNumber = await this.isUnique(serialNumber);
+
+      if (!isUniqueSerialNumber)
+        errors["serialNumber"] = "serialNumber must be unique";
+    }
+
+    if (this.isEmpty(name)) {
+      errors["name"] = "name field is required";
+    }
+
+    if (this.isEmpty(ipV4)) {
+      errors["ipV4"] = "ipV4 field is required";
+    }
+
+    if (ipV4 && !this.isIpV4(ipV4)) {
+      errors["ipV4"] = "Required valid IpV4";
+    }
+
+    return {
+      errors,
+      isValid: this.isEmpty(errors),
+    };
+  };
+}
