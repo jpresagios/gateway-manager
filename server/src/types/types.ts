dd668ba626f6956c12e7a2f842e45c3a475df52f
Junior <junior.presagios@gmail.com>
2020-08-22 17:02:27 -0400

    Added GateWayValidator to types to used on Inversify

--- server/src/types/types.ts
+++ server/src/types/types.ts
@@ -1,6 +1,7 @@
 const TYPES = {
   GateWayRepository: Symbol.for("GateWayRepository"),
   DeviceRepository: Symbol.for("DeviceRepository"),
+  GateWayValidator: Symbol.for("GateWayValidator"),
 };
 
 export { TYPES };
