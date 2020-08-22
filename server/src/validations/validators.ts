b46efe61582757345ad14f84eb9bde256481d3e5
Junior <junior.presagios@gmail.com>
2020-08-22 16:58:00 -0400

    Added base Validator
    This Validator contain common Validations functionality

--- server/src/validations/validators.ts
+++ server/src/validations/validators.ts
@@ -1,15 +1,20 @@
-const isEmpty = (value) => {
-  return (
-    value === undefined ||
-    value === null ||
-    (typeof value === "object" && Object.keys(value).length === 0) ||
-    (typeof value === "string" && value.trim().length === 0)
-  );
-};
+import "reflect-metadata";
+import { injectable } from "inversify";
 
-const isIpV4 = (value) => {
-  const ipV4Regex = /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-  return ipV4Regex.test(value);
-};
+@injectable()
+export default class Validator {
+  /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+  isEmpty = (value): boolean => {
+    return (
+      value === undefined ||
+      value === null ||
+      (typeof value === "object" && Object.keys(value).length === 0) ||
+      (typeof value === "string" && value.trim().length === 0)
+    );
+  };
 
-export { isEmpty, isIpV4 };
+  isIpV4 = (value: string): boolean => {
+    const ipV4Regex = /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+    return ipV4Regex.test(value);
+  };
+}
