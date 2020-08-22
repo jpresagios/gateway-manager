933bad8a0243243e9ab425cd43194009bb702062
Junior <junior.presagios@gmail.com>
2020-08-22 17:48:32 -0400

    Added DeviceValidator types for inversify

--- server/src/types/types.ts
+++ server/src/types/types.ts
@@ -2,6 +2,7 @@ const TYPES = {
   GateWayRepository: Symbol.for("GateWayRepository"),
   DeviceRepository: Symbol.for("DeviceRepository"),
   GateWayValidator: Symbol.for("GateWayValidator"),
+  DeviceValidator: Symbol.for("DeviceValidator"),
 };
 
 export { TYPES };
