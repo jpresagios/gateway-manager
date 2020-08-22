17ef8fa15246a74ac63a08a13fa23c2189ec0f47
Junior <junior.presagios@gmail.com>
2020-08-22 12:34:02 -0400

    Added validators

---
+++ server/src/validations/validators.ts
@@ -0,0 +1,15 @@
+const isEmpty = (value) => {
+  return (
+    value === undefined ||
+    value === null ||
+    (typeof value === "object" && Object.keys(value).length === 0) ||
+    (typeof value === "string" && value.trim().length === 0)
+  );
+};
+
+const isIpV4 = (value) => {
+  const ipV4Regex = /(([0-1]?[0-9]{1,2}\.)|(2[0-4][0-9]\.)|(25[0-5]\.)){3}(([0-1]?[0-9]{1,2})|(2[0-4][0-9])|(25[0-5]))/;
+  return ipV4Regex.test(value);
+};
+
+export { isEmpty, isIpV4 };
