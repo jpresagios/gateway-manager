ee3c5ae61d2028d39346dae6c8bfe738d7c94921
Junior <junior.presagios@gmail.com>
2020-08-22 13:25:20 -0400

    Added routes on Root component

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import NavBar from "./components/commons/NavBar";
+import GateWayList from "./components/GateWayList";
+import GateWayForm from "./components/GateWay/GateWayForm";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <div className="container mt-5">
+        <Route exact path="/" component={GateWayList} />
+        <Route exact path="/gateway/add" component={GateWayForm} />
+      </div>
+    </Router>
   );
 }
 
