ff7aa27b6c6a8d902de42bfda664abee3fe24621
Junior <junior.presagios@gmail.com>
2020-08-22 15:45:21 -0400

    Added Detail gateWay

--- client/src/components/GateWayList.js
+++ client/src/components/GateWayList.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Collapse, CardBody, Card, CardHeader, Button } from "reactstrap";
+import { Collapse, CardBody, Card, CardHeader } from "reactstrap";
 import { gateWayList } from "../api/gateway";
 import DeviceList from "./Device/DeviceList";
 import { Link } from "react-router-dom";
@@ -42,6 +42,13 @@ class GateWayList extends Component {
             <Card style={{ marginBottom: "1rem" }} key={index}>
               <CardHeader onClick={this.toggle} data-event={index}>
                 {gateway.serialNumber}
+
+                <Link
+                  to={"/gateway/detail/" + gateway._id}
+                  className="float-right"
+                >
+                  Detail
+                </Link>
               </CardHeader>
               <Collapse isOpen={collapse === index}>
                 <CardBody>
