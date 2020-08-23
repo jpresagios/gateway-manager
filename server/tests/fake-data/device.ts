76f75df974d594c26dc386f9177eef0e0f6cc88c
Junior <junior.presagios@gmail.com>
2020-08-22 22:08:17 -0400

    Added test to validate method getNumberDevice

---
+++ server/tests/fake-data/device.ts
@@ -0,0 +1,12 @@
+export default [
+  {
+    uid: 1,
+    vendor: "Vendor  1",
+    status: "online",
+  },
+  {
+    uid: 3,
+    vendor: "Vendor  2",
+    status: "offline",
+  },
+];
--- server/tests/gateway-repository.test.ts
+++ server/tests/gateway-repository.test.ts
@@ -3,7 +3,9 @@ import {
   clearDatabase,
   closeDatabase,
 } from "./mongodb-test-enviroment";
+
 import gateWayData from "./fake-data/gateway";
+import deviceData from "./fake-data/device";
 
 // Connect to a new in-memory database before running any tests.
 beforeAll(async () => await connect());
@@ -16,6 +18,7 @@ afterAll(async () => await closeDatabase());
 
 import GateWay from "../src/models/gateways.model";
 import GateWayRepository from "../src/repositories/gateway-repository";
+import DeviceRepository from "../src/repositories/device-repository";
 
 describe("GateWay Repository", () => {
   test("Method insertGateWay Successfully insert GateWays on Database", async () => {
@@ -90,5 +93,22 @@ describe("GateWay Repository", () => {
     expect(name).toEqual(result.name);
 
     expect(ipV4).toEqual(result.ipV4);
+  });
+
+  test("Method getNumberDevice Return correct number devices assocated to a gateway", async () => {
+    const gateWayRepository = new GateWayRepository();
+    const deviceRepository = new DeviceRepository();
+
+    const result = await gateWayRepository.insertGateWay(gateWayData[0]);
+
+    for (let i = 0; i < deviceData.length; i++) {
+      await deviceRepository.addDevice(result._id, deviceData[i]);
+    }
+
+    const countGateWays: number = await gateWayRepository.getNumberDevice(
+      result._id
+    );
+
+    expect(countGateWays).toEqual(deviceData.length);
   });
 });
