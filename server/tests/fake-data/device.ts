2e812790bd04ff1b8da3b21a90c7831b2db6cffe
Junior <junior.presagios@gmail.com>
2020-08-23 09:38:38 -0400

    Added device fake data

--- server/tests/fake-data/device.ts
+++ server/tests/fake-data/device.ts
@@ -9,4 +9,44 @@ export default [
     vendor: "Vendor  2",
     status: "offline",
   },
+  {
+    uid: 5,
+    vendor: "Vendor  3",
+    status: "offline",
+  },
+  {
+    uid: 7,
+    vendor: "Vendor  4",
+    status: "offline",
+  },
+  {
+    uid: 8,
+    vendor: "Vendor  5",
+    status: "online",
+  },
+  {
+    uid: 9,
+    vendor: "Vendor  6",
+    status: "online",
+  },
+  {
+    uid: 666,
+    vendor: "Vendor  7",
+    status: "offline",
+  },
+  {
+    uid: 10,
+    vendor: "Vendor  8",
+    status: "offline",
+  },
+  {
+    uid: 10,
+    vendor: "Vendor  9",
+    status: "online",
+  },
+  {
+    uid: 11,
+    vendor: "Vendor  10",
+    status: "offline",
+  },
 ];
