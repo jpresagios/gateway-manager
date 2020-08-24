91b6cda59ea9b4995e1bb4b3748202fc3339e511
Junior <junior.presagios@gmail.com>
2020-08-23 22:05:45 -0400

    Added fake data for gatewayList

---
+++ client/test/fake-data/gateway.js
@@ -0,0 +1,47 @@
+export default {
+  data: [
+    {
+      devices: [
+        {
+          _id: "5f4312a5c208b22674d07b81",
+          uid: 2,
+          vendor: "Vendor  2",
+          status: "offline",
+          createAt: "2020-08-24T01:06:45.653Z",
+          __v: 0,
+        },
+        {
+          _id: "5f4312a5c208b22674d07b82",
+          uid: 3,
+          vendor: "Vendor  3",
+          status: "offline",
+          createAt: "2020-08-24T01:06:45.656Z",
+          __v: 0,
+        },
+      ],
+      _id: "5f4312a5c208b22674d07b7f",
+      serialNumber: "ABCD",
+      name: "GateWay 1",
+      ipV4: "69.89.31.226",
+      __v: 0,
+    },
+    {
+      devices: [
+        {
+          _id: "5f4312a5c208b22674d07b85",
+          uid: 2,
+          vendor: "Vendor  2",
+          status: "offline",
+          createAt: "2020-08-24T01:06:45.664Z",
+          __v: 0,
+        },
+      ],
+      _id: "5f4312a5c208b22674d07b83",
+      serialNumber: "zFe345",
+      name: "GateWay 2",
+      ipV4: "102.4.31.226",
+      __v: 0,
+    },
+  ],
+  success: true,
+};
