d6519c5905d987714923021d8a47277d0d94fc58
Junior <junior.presagios@gmail.com>
2020-08-23 21:46:58 -0400

    Added jest config

---
+++ client/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  testEnvironment: "jsdom",
+  moduleDirectories: ["node_modules", "src"],
+  testMatch: ["**/test/**/*.test.js"],
+  verbose: true,
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+};
